2b6fb149979f9b3ccb4aaa90c4b994e15344a50f
Augustus Boling <aboling@proofpoint.com>
2017-05-24 16:40:31 -0700

    Added project2 directory, main script (unfinished)

---
+++ project2/main.py
@@ -0,0 +1,14 @@
+'''
+Augustus Boling
+COEN 169 - Web Information Management
+24 May 2017
+'''
+
+import sys
+
+def main():
+    print("Hello World!")
+    sys.exit(0)
+
+if __name__=="__main__":
+    main()
