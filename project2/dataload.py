cc4436464b9c9da6102b673fb44d6c061c655c40
gusboling <geekish2428@gmail.com>
2017-05-25 13:33:18 -0700

    All data can now be retrieved from files; moving on to analysis

--- project2/dataload.py
+++ project2/dataload.py
@@ -13,27 +13,54 @@ import sys
 #Application imports
 import models
 
+#Function: training_data
 #Returns a list of TrainUser objects
-def TrainingData(file_path):
+def training_data(file_path):
     trainers = []
 
     with open(file_path, "r") as train_file:
         raw_ratings = train_file.readline().split()
+        #for every 1K ratings, create a new user and append it to the list
         for i in range((len(raw_ratings)/1000)):
             user_ratings = raw_ratings[:1000]
-            trainers.append(models.User(i, user_ratings))
+            trainers.append(models.TrainUser(i, user_ratings))
             raw_ratings = raw_ratings[1000:]
 
     return trainers
 
-#Returns a list of TestUser objects
-def TestData(file_path):
+#Function: test_data
+#Returns a list of TestUser objects (used for all three test files)
+def test_data(file_path):
     testers = []
+    users = {}
 
     with open(file_path, "r") as test_file:
+        #for each line in test_file, split it and map the last 2 values (movie ID, rating) to the first value (user ID)
         for line in test_file:
-            ln_elements = line.split()
-            groups[ln_elements[0]].append(ln_elements[0:])
-            
+            tokens = line.split()
+            user_id = tokens[0]
+            movie_id = tokens[1]
+            rating = tokens[2]
+
+            if user_id not in users:
+                users[user_id] = models.TestUser(user_id, {movie_id:rating})
+            else:
+                users[user_id].add_rating(movie_id, rating)
+
+    #Convert to list
+    for key in users:
+        testers.append(users[key])
 
     return testers
+
+#Debugging Code - run when module is called from the command line w/ no arguments.
+if __name__=="__main__":
+    print("[DEBUG] Starting debugging execution for dataload.py")
+
+    print("[DEBUG] Loading training data.")
+    train_users = training_data("./train.txt")
+    print("[DEBUG] Loaded {} training user profiles from 'train.txt'".format(len(train_users)))
+
+    print("[DEBUG] Loading test5 data.")
+    test_users = test_data("./test5.txt")
+    print("[DEBUG] Loaded {} testing user profiles from 'test5.txt'.".format(len(test_users)))
--- project2/models.py
+++ project2/models.py
@@ -12,9 +12,9 @@ both the test and training data using python classes.
 import sys
 
 class TrainUser:
-    #Constructor method
+    #Constructor method; takes a user id (string) and a dict of <movie>:<rating> pairs.
     def __init__(self, new_id, new_rating_list):
-        self.id = new_id
+        self.id = str(new_id)
         self.ratings = {}
         for index, rating in enumerate(new_rating_list):
             self.ratings[index] = rating
@@ -26,17 +26,22 @@ class TrainUser:
         return 0;
 
 class TestUser:
-    #Constructor method
+    #Constructor method; takes a user id (string) and a dict of <movie>:<rating> pairs.
     def __init__(self, new_id, new_ratings):
-        self.id = new_id
+        self.id = str(new_id)
         self.targets = [] #The movies we want to make predictions about [<movie_id>]
         self.ratings = new_ratings #A dictionary of all rated movies {<movie_id>:<movie_rating>}
         for key in new_ratings:
-            if ratings[key] == 0:
+            if new_ratings[key] == 0:
                 self.targets.append(key)
 
     #Return the rating of movie at ratings[movie_id]; returns 0 if key not valid.
     def get_rating(self, movie_id):
         if movie_id in self.ratings:
             return self.ratings[movie_id]
-        return 0; 
+        return 0;
+
+    def add_rating(self, movie_id, rating):
+        self.ratings[movie_id] = rating
+        if rating == 0:
+            self.targets.append(movie_id)
